d96f7839ce5724c4e61e80fb2870f7bc9921e7eb
tinnist <82404492+tinnist@users.noreply.github.com>
2021-05-17 13:56:45 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,79 @@
-
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        jump_track = 0
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (jump_track < 2) {
+        mySprite.vy = -100
+    }
+    jump_track += 1
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava0, function (sprite, location) {
+    mySprite.destroy()
+    info.changeLifeBy(-1)
+    mySprite = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(mySprite, 100, 0)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(3, 2))
+    scene.cameraFollowSprite(mySprite)
+    statusbar.attachToSprite(mySprite)
+})
+function hard_path () {
+    tiles.setTilemap(tilemap`level2`)
+}
+function less_hard_path () {
+	
+}
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleRedCrystal, function (sprite, location) {
+    hard_path()
+})
+let statusbar: StatusBarSprite = null
+let jump_track = 0
+let mySprite: Sprite = null
+tiles.setTilemap(tilemap`level1`)
+mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+game.showLongText("This is the first part of my masterpiece. Dont worry, I promise that its possible!", DialogLayout.Bottom)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(3, 2))
+controller.moveSprite(mySprite, 100, 0)
+scene.cameraFollowSprite(mySprite)
+jump_track = 0
+info.setLife(2)
+statusbar = statusbars.create(20, 4, StatusBarKind.Health)
+statusbar.attachToSprite(mySprite)
+forever(function () {
+    mySprite.ay = 200
+})
